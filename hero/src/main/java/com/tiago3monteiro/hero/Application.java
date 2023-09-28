60dea6fc1cde5ffbee8cb33a82c0630831e0a13f
“Tiago <up202108391@example.fe.up.pt>
2023-09-28 15:58:07 +0100

    6.Creating Game(tive de recomeçar que nao estava facil)

---
+++ hero/src/main/java/com/tiago3monteiro/hero/Application.java
@@ -0,0 +1,10 @@
+package com.tiago3monteiro.hero;
+import java.io.IOException;
+
+public class Application {
+    public static void main(String[] args) throws IOException {
+        Game game = new Game();
+        game.run();
+    }
+
+}
---
+++ hero/src/main/java/com/tiago3monteiro/hero/Game.java
@@ -0,0 +1,43 @@
+package com.tiago3monteiro.hero;
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.input.KeyType;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+import com.googlecode.lanterna.input.KeyStroke;
+
+import java.io.IOException;
+
+public class Game {
+
+    private Screen screen;
+    private int x = 10;
+    private int y = 10;
+
+    public Game() throws IOException {
+        TerminalSize terminalSize = new TerminalSize(40, 20);
+        DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory().setInitialTerminalSize(terminalSize);
+        Terminal terminal = new DefaultTerminalFactory().createTerminal();
+        screen = new TerminalScreen(terminal);
+        screen.setCursorPosition(null);
+        screen.startScreen();
+        screen.doResizeIfNecessary();
+
+    }
+
+    private void draw() throws IOException {
+        screen.clear();
+
+        screen.setCharacter(x, y, TextCharacter.fromCharacter('X')[0]);
+        screen.refresh();
+    }
+
+    public void run() throws IOException {
+            this.draw();
+
+        }
+    }
+
+
